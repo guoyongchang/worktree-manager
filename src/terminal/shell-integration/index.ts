8ee9c4513834c3779b438480ea519c0057fb6b8f
guoyongchang <guoyongchang@chanyi.vip>
2026-06-12 07:07:13 +0800

    test: commit

--- src/terminal/shell-integration/index.ts
+++
@@ -1,7 +0,0 @@
-// src/terminal/shell-integration/index.ts
-
-export { OscParser } from './osc-parser'
-export { CommandDetection } from './command-detection'
-export { CommandDecorationAddon } from './command-decoration'
-export { CommandState } from './types'
-export type { CommandInfo, OscEvent } from './types'
